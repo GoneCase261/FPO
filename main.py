0c3549ee71202a14e34eb4eb63d48c26d945b2b4
GoneCase <poonamhedau15@gmail.com>
2025-12-17 23:44:06 +0530

    w10- tire compound, nd fuel depletion chart nd df

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 # st.set_page_config(layout="wide")
 
 if "tire_wear" not in st.session_state:
     st.session_state.tire_wear = 0
 
-st.title('🏎️ F1 Pit Optimizer v1.0 - Week 8')
+st.title('🏎️ F1 Pit Optimizer v1.0 - Week 10')
 lap_no = st.slider('Lap Progress:', 0, 60, 0)
 
 with open('canvas.html', 'r', encoding='utf-8') as c:
@@ -49,8 +50,10 @@ tire_comp = st.selectbox(
 rain = st.slider("Rain", 0.0, 1.0, 0.0, 0.1)
 
 
-def lap_time(w, tc, r):
+def lap_time(w, tc, r, f):
     base_time = 90
+    fuel_penalty = f * 0.03  # 0.03-> fuel factor(0.3/10)seconds per kg
+    # About 0.25–0.35 seconds per 10 kg of fuel per lap.
     if 0 <= w <= 30:
         lt = base_time
     elif 30 < w <= 70:
@@ -60,6 +63,7 @@ def lap_time(w, tc, r):
         penalty = 4  # 70-30 * 0.1 for penalty at 70
         extra = (w-70)*0.3
         lt = base_time+penalty+extra
+    lt += fuel_penalty
 
 # BASE TIME FOR EACH TYRE TYPE (WITHOUT RAIN)
     if tc == "SOFT":  # tire compound factor
@@ -86,7 +90,7 @@ def lap_time(w, tc, r):
 
 
 # realtime based on slider
-st.metric("LAP TIME:", f"{lap_time(wear(lap_no), tire_comp, rain)} s")
+st.metric("LAP TIME:", f"{lap_time(wear(lap_no), tire_comp, rain, 110)} s")
 
 # Slider + metrics showing “current lap” wear/time.
 # Selectbox controlling pit lap for the full‑race simulation and chart
@@ -95,12 +99,16 @@ st.metric("LAP TIME:", f"{lap_time(wear(lap_no), tire_comp, rain)} s")
 def race_simulation(p1, p2, pit_penalty=25):
     total_time = 0
     w = 0  # fresh tyres at every new race simulation
+    fuel = 110  # (kg)
     time_record = []
     lap_n = []
+    fuel_left = []
     for lap in range(1, 61):
         # purely linear wear increase, not based on wear function
         w = min(w+2, 100)
-        laptime = lap_time(w, tire_comp, rain)
+        fuel -= 110/60
+        fuel_left.append(fuel)
+        laptime = lap_time(w, tire_comp, rain, fuel)
         total_time += laptime
         if p1 == p2 and p1 != 0:
             if p1 == lap:
@@ -117,14 +125,15 @@ def race_simulation(p1, p2, pit_penalty=25):
 
         lap_n.append(lap)
         time_record.append(total_time)
-    return total_time, lap_n, time_record  # cummulative time after every lap
+    # cummulative time after every lap
+    return total_time, lap_n, time_record, fuel_left
 
 
 pits = []
 time = []
 for pit1 in range(0, 61):
     for pit2 in range(0, 61):
-        t, ln, tr = race_simulation(pit1, pit2)
+        t, ln, tr, fl = race_simulation(pit1, pit2)
         pits.append(f"({pit1}, {pit2})")
         time.append(t)
 
@@ -134,3 +143,10 @@ b = pits[time.index(a)]
 d = pits[time.index(c)]
 st.write(f"Overall best pit strategy is : {b} (total {a:.2f} s). ")
 st.write(f"This is {c-a:.2f} s faster than pit at {d}.")
+
+df = pd.DataFrame({
+    'Lap': ln,
+    'Fuel': fl
+})
+st.write(df)
+st.line_chart(df.set_index('Lap'))
